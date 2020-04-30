e1d444bc747d98ed14ab9abb9406d70387dd1ef9
MCC <machangchang@yunteck.cn>
2020-04-30 10:48:52 +0800

    Initial commit

---
+++ src/main/java/com/example/juc/demo/sync/PhaserTest1.java
@@ -0,0 +1,39 @@
+package com.example.juc.demo.sync;
+
+import java.util.concurrent.Phaser;
+
+/**
+ * @Description
+ * @Author MCC
+ * @Date 2020/4/30 9:41
+ * @Version 1.0
+ **/
+public class PhaserTest1 {
+    public static void main(String[] args) {
+        Phaser phaser = new Phaser();
+        for (int i = 0; i < 10; i++) {
+            phaser.register();                  // 注册各个参与者线程
+            System.out.println("register");
+            Thread thread = new Thread(new Task(phaser), "Thread-" + i);
+            thread.start();
+            if (i == 3) {
+                thread.interrupt();
+            }
+        }
+    }
+}
+
+class Task implements Runnable {
+    private final Phaser phaser;
+
+    Task(Phaser phaser) {
+        this.phaser = phaser;
+    }
+
+    @Override
+    public void run() {
+        int i = phaser.arriveAndAwaitAdvance();     // 等待其它参与者线程到达
+        // do something
+        System.out.println(Thread.currentThread().getName() + ": 执行完任务，当前phase =" + i + "");
+    }
+}
